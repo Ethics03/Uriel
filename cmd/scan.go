dd0e77bfca8353c62191d8973c09e81b9d8f7727
Rachit Srivastava <honey032004@gmail.com>
2025-05-06 23:09:00 +0530

    feat: cli check

--- cmd/scan.go
+++ cmd/scan.go
@@ -12,13 +12,9 @@ import (
 // scanCmd represents the scan command
 var scanCmd = &cobra.Command{
 	Use:   "scan",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Your personal API-Security Tester",
+	Long: `Uriel is an API-Security Testing CLI-Tool that uses 
+	Llama-3 with tailored checks for seamless API performance.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Uriel is scanning your API.")
 	},
