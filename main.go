3cad3295fba2e64b0ef3a3a05d483122badfd546
Rachit Srivastava <honey032004@gmail.com>
2025-05-08 23:20:25 +0530

    feat: added prompt structure

--- main.go
+++ main.go
@@ -4,6 +4,17 @@ import (
 	"github.com/Ethics03/Uriel/cmd"
 )
 
+type Request struct {
+	Model    string    `json:"model"`
+	Messages []Message `json:"messages"`
+	Stream   bool      `json:"stream"`
+}
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
 func main() {
 	cmd.Execute()
 }
